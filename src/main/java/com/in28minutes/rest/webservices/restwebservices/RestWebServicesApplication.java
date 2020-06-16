6ae33c08895d04b5e20680549129c1c43a624588
sandy121singh <singh18sandy@gmail.com>
2020-06-17 01:19:52 +0530

    Adding Logger to project

--- src/main/java/com/in28minutes/rest/webservices/restwebservices/RestWebServicesApplication.java
+++ src/main/java/com/in28minutes/rest/webservices/restwebservices/RestWebServicesApplication.java
@@ -1,12 +1,24 @@
 package com.in28minutes.rest.webservices.restwebservices;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestWebServicesApplication {
+	
+	private static final Logger logger = LoggerFactory.getLogger(RestWebServicesApplication.class);
 
+	public void init() {
+		
+		logger.info("Starting the applicaiton...");
+	}
+	
 	public static void main(String[] args) {
+		
+		logger.info("Spring Boot Application Executing...");
+		
 		SpringApplication.run(RestWebServicesApplication.class, args);
 	}
 
--- src/test/java/com/in28minutes/rest/webservices/restwebservices/RestWebServicesApplicationTests.java
+++ src/test/java/com/in28minutes/rest/webservices/restwebservices/RestWebServicesApplicationTests.java
@@ -1,13 +1,21 @@
 package com.in28minutes.rest.webservices.restwebservices;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RestWebServicesApplicationTests {
-
+	
+	private static final Logger logger=LoggerFactory.getLogger(RestWebServicesApplicationTests.class);
+	
 	@Test
 	void contextLoads() {
+		logger.info("Test Executing....");
+		assertEquals(true, true);
 	}
 
 }
